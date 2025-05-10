b00054abf771fcc7b51c6c68e406589abc5f5ceb
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:05:28 +0200

    improve `configuration.ts`

--- services/s2MetricsProvider/src/configuration.ts
+++ services/s2MetricsProvider/src/configuration.ts
@@ -2,6 +2,9 @@ import { ConfigObject } from '@nestjs/config';
 
 export default (): ConfigObject => {
   return {
+    nodeEnv: process.env.NODE_ENV,
+    packageName: process.env.npm_package_name,
+    port: parseInt(process.env.PORT),
     mongoDbUriFlip: process.env.MONGODB_URI_FLIP.replace('?????', process.env.NODE_ENV),
   };
 };
