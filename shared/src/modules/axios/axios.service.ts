a2d0868347acf7ef5108dc742065ec26f67a9092
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:05:04 +0200

    improve `AxiosService.request`

--- shared/src/modules/axios/axios.service.ts
+++ shared/src/modules/axios/axios.service.ts
@@ -21,21 +21,30 @@ export class AxiosService {
       const result = await this.getAxios().request<T>(options);
       const duration = Date.now() - startTime;
 
-      this.consoleLogger.log(`${result.status} ${result.statusText} - ${duration} ms`, AxiosService.name);
-      this.consoleLogger.log('Request details', options, AxiosService.name);
+      this.consoleLogger.log(
+        `Outgoing request: ${result.status} ${result.statusText} - ${duration} ms`,
+        AxiosService.name,
+      );
+      this.consoleLogger.log(
+        `Content length: ${result.headers['content-length']}`,
+        'Request details',
+        options,
+        AxiosService.name,
+      );
 
       return result.data;
     } catch (error) {
       const duration = Date.now() - startTime;
 
       this.consoleLogger.error(
-        `${error.constructor.name}: Outgoing request ${error.response.data.statusCode} ${error.code} - ${duration} ms`,
+        `${error.constructor.name} - Outgoing request: ${error.response.data.statusCode} ${error.code} - ${duration} ms`,
         AxiosService.name,
       );
       this.consoleLogger.error(
+        `Content length: ${error.response.headers['content-length']}`,
         'Request details',
         options,
-        'Request response data',
+        'Request response details',
         error.response.data,
         AxiosService.name,
       );
