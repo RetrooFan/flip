f101154b6ed4839d7a2a6f4d28d77bd0791c33be
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:02:51 +0200

    add logger

--- services/s1DataAnalyzer/src/main.ts
+++ services/s1DataAnalyzer/src/main.ts
@@ -1,11 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FlipExceptionFilter } from '../../../shared/src/exceptionFilters/flipException.filter';
+import { logger } from '../../../shared/src/middlewares/logger.middleware';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   console.log(`Container name: ${process.env.npm_package_name}`);
   const app = await NestFactory.create(AppModule);
+  app.use(logger);
   app.useGlobalFilters(new FlipExceptionFilter(app));
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get<number>('port');
