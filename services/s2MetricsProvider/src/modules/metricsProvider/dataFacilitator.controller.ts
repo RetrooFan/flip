7e9bbcb12adc3b3e216c3160ea0aa7c4d0665b30
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:05:23 +0200

    improve `MetricsProviderModule`

--- services/s2MetricsProvider/src/modules/metricsProvider/dataFacilitator.controller.ts
+++ services/s2MetricsProvider/src/modules/metricsProvider/dataFacilitator.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Product } from '../../../../../shared/src/classes/product.class';
 import { MetricsProviderService } from './metricsProvider.service';
 
 @Controller()
@@ -6,17 +7,23 @@ export class MetricsProviderController {
   constructor(private readonly metricsProviderService: MetricsProviderService) {}
 
   @Get('top10SalesValue')
-  private async top10SalesValue(): Promise<void> {
+  private async top10SalesValue(): Promise<Product[]> {
     this.metricsProviderService;
+
+    return [];
   }
 
   @Get('top10OrderCount')
-  private async top10OrderCount(): Promise<void> {
+  private async top10OrderCount(): Promise<Product[]> {
     this.metricsProviderService;
+
+    return [];
   }
 
   @Get('top10OrderCountYesterday')
-  private async top10OrderCountYesterday(): Promise<void> {
+  private async top10OrderCountYesterday(): Promise<Product[]> {
     this.metricsProviderService;
+
+    return [];
   }
 }
--- services/s2MetricsProvider/src/modules/metricsProvider/metricsProvider.service.ts
+++ services/s2MetricsProvider/src/modules/metricsProvider/metricsProvider.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Product } from '../../../../../shared/src/classes/product.class';
 import { MetricsOfProduct, MetricsOfProductDocument } from '../../../../../shared/src/entities/metricsOfProduct.entity';
 import { DbConnection } from '../../../../../shared/src/enums/dbConnection.enum';
 
@@ -11,15 +12,21 @@ export class MetricsProviderService {
     private readonly metricsOfProductModel: Model<MetricsOfProductDocument>,
   ) {}
 
-  public async top10SalesValue(): Promise<void> {
+  public async top10SalesValue(): Promise<Product[]> {
     this.metricsOfProductModel;
+
+    return [];
   }
 
-  public async top10OrderCount(): Promise<void> {
+  public async top10OrderCount(): Promise<Product[]> {
     this.metricsOfProductModel;
+
+    return [];
   }
 
-  public async top10OrderCountYesterday(): Promise<void> {
+  public async top10OrderCountYesterday(): Promise<Product[]> {
     this.metricsOfProductModel;
+
+    return [];
   }
 }
