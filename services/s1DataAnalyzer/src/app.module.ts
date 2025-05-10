a0fb2e8cc4187665fb5ccfabc08b4683e2d7befb
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:45:34 +0200

    import `ConfigModule`

--- services/s1DataAnalyzer/src/app.module.ts
+++ services/s1DataAnalyzer/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HelloModule } from '../../../shared/src/modules/hello/hello.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [ConfigModule.forRoot(), HelloModule],
 })
 export class AppModule {}
