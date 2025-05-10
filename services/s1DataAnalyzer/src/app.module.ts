49d72ebc09c847368bee6ad4de59d2fbde09639a
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:48:06 +0200

    import configuration

--- services/s1DataAnalyzer/src/app.module.ts
+++ services/s1DataAnalyzer/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HelloModule } from '../../../shared/src/modules/hello/hello.module';
+import configuration from './configuration';
 import { DataAnalyzerModule } from './modules/dataAnalyzer/dataAnalyzer.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HelloModule, DataAnalyzerModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] }), HelloModule, DataAnalyzerModule],
 })
 export class AppModule {}
