d75cba715c7a514cd53cd043fd37b17f7d49a766
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:48:02 +0200

    improve `configuration.ts`

--- services/s0DataProvider/src/configuration.ts
+++ services/s0DataProvider/src/configuration.ts
@@ -3,9 +3,9 @@ import { ConfigObject } from '@nestjs/config';
 export default (): ConfigObject => {
   return {
     nodeEnv: process.env.NODE_ENV,
-    port: parseInt(process.env.PORT as string, 10) || 80,
+    port: parseInt(process.env.PORT),
     dataSourceApi: process.env.DATA_SOURCE_API,
-    itemsNumberQueryLimit: process.env.ITEMS_NUMBER_QUERY_LIMIT,
-    mongoDbUriFlip: process.env.MONGODB_URI_FLIP,
+    itemsNumberQueryLimit: parseInt(process.env.ITEMS_NUMBER_QUERY_LIMIT),
+    mongoDbUriFlip: process.env.MONGODB_URI_FLIP.replace('?????', process.env.NODE_ENV),
   };
 };
--- services/s1DataAnalyzer/src/configuration.ts
+++ services/s1DataAnalyzer/src/configuration.ts
@@ -3,11 +3,11 @@ import { ConfigObject } from '@nestjs/config';
 export default (): ConfigObject => {
   return {
     nodeEnv: process.env.NODE_ENV,
-    port: parseInt(process.env.PORT as string, 10) || 80,
+    port: parseInt(process.env.PORT),
     dataAnalyzerCronTime: process.env.DATA_ANALYZER_CRON_TIME,
-    mongoDbUriFlip: process.env.MONGODB_URI_FLIP,
+    mongoDbUriFlip: process.env.MONGODB_URI_FLIP.replace('?????', process.env.NODE_ENV),
     productYesterdayCounterTime: process.env.PRODUCT_YESTERDAY_COUNTER_TIME,
     redisUrl: process.env.REDIS_URL,
-    redisPort: process.env.REDIS_PORT,
+    redisPort: parseInt(process.env.REDIS_PORT),
   };
 };
