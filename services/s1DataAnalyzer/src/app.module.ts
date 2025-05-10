d47059e397b1676a4c4acb8a95b9c4d26905ad1f
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:45:47 +0200

    rename `DataAnalyzerModule`

--- services/s1DataAnalyzer/src/app.module.ts
+++ services/s1DataAnalyzer/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HelloModule } from '../../../shared/src/modules/hello/hello.module';
+import { DataAnalyzerModule } from './modules/dataAnalyzer.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HelloModule],
+  imports: [ConfigModule.forRoot(), HelloModule, DataAnalyzerModule],
 })
 export class AppModule {}
