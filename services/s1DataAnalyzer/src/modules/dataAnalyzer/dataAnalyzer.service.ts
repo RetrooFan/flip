509837736f52c916039c4d0ef6516eacfa083649
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:46:59 +0200

    improve `DataAnalyzerService`

--- services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.service.ts
+++ services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.service.ts
@@ -5,13 +5,15 @@ import { S1DataAnalyzerError } from '../../errors/s1DataAnalyzer.error';
 
 @Injectable()
 export class DataAnalyzerService {
+  private message = 'BLA';
+
   constructor(cronService: CronService) {
     cronService.addJob(DataAnalyzerService.name, process.env.DATA_ANALYZER_CRON_TIME, () =>
-      errorRethrower(analyzeData(), S1DataAnalyzerError),
+      errorRethrower(this.analyzeData(), S1DataAnalyzerError),
     );
   }
-}
 
-async function analyzeData(): Promise<void> {
-  console.log('BLA', DataAnalyzerService.name);
+  public async analyzeData(): Promise<void> {
+    console.log(this.message, DataAnalyzerService.name);
+  }
 }
