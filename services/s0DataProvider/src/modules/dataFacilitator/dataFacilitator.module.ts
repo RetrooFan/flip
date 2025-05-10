34095e04e7a8f02b8e2ada1cee53717d036e5419
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:43:14 +0200

    fill in `DataFacilitatorModule`

--- services/s0DataProvider/src/modules/dataFacilitator/dataFacilitator.module.ts
+++ services/s0DataProvider/src/modules/dataFacilitator/dataFacilitator.module.ts
@@ -1,4 +1,29 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, orderSchema } from '../../../../../shared/src/entities/order.entity';
+import { DbConnection } from '../../../../../shared/src/enums/dbConnection.enum';
+import { DataFacilitatorController } from './dataFacilitator.controller';
+import { DataFacilitatorService } from './dataFacilitator.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI_FLIP,
+      }),
+      connectionName: DbConnection.DataFacilitator,
+    }),
+    MongooseModule.forFeature(
+      [
+        {
+          name: Order.name,
+          schema: orderSchema,
+        },
+      ],
+      DbConnection.DataFacilitator,
+    ),
+  ],
+  controllers: [DataFacilitatorController],
+  providers: [DataFacilitatorService],
+})
 export class DataFacilitatorModule {}
