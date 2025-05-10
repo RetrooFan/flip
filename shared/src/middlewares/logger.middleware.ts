07f7a5c5d596b4c36ceebe677f7d4f8ccba53076
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:04:58 +0200

    improve `logger` middleware

--- shared/src/middlewares/logger.middleware.ts
+++ shared/src/middlewares/logger.middleware.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 
+let counter = 0;
+
 export function logger(req: Request, res: Response, next: NextFunction): void {
   const request = {
-    ip: req.ip,
     method: req.method,
     url: req.url.split('?')[0],
     query: req.query,
     params: req.params,
+    ip: req.ip,
   };
 
-  console.log('Request', request);
+  console.log('Request', counter++, request);
   console.log();
 
   next();
