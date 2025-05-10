a22823a78f773079e233c642c7527ca3cdb42bcf
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:48:27 +0200

    improve `CronService`

--- shared/src/modules/cron/cron.service.ts
+++ shared/src/modules/cron/cron.service.ts
@@ -6,18 +6,25 @@ import { CronJob } from 'cron';
 export class CronService {
   constructor(private readonly schedulerRegistry: SchedulerRegistry, private readonly consoleLogger: ConsoleLogger) {}
 
-  addJob(name: string, cronTime: string, trigger: () => Promise<void>): void {
+  addJob(name: string, cronTime: string, callback: () => Promise<void>): void {
     const messageAdd = `Cron job ${name} added with [${cronTime}]`;
     const messageExecuteStarted = `Cron job ${name} execution started`;
     const messageExecuteFinished = `Cron job ${name} execution finished`;
 
-    const job = new CronJob(cronTime, async () => {
+    const callbackHandled = async (): Promise<void> => {
       this.consoleLogger.log(messageExecuteStarted, CronService.name);
 
-      await trigger();
+      try {
+        await callback();
+      } catch (error) {
+        const message = [error.constructor.name, error.stack].join('\n');
+        this.consoleLogger.error(message, CronService.name);
+      }
 
       this.consoleLogger.log(messageExecuteFinished, CronService.name);
-    });
+    };
+
+    const job = new CronJob(cronTime, callbackHandled);
 
     this.schedulerRegistry.addCronJob(name, job);
     job.start();
