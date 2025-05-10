b25bb208921760be9690f912c560108116ff9116
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:46:21 +0200

    remove unnecessary properties

--- services/s1DataAnalyzer/src/modules/dataAnalyzer.module.ts
+++ services/s1DataAnalyzer/src/modules/dataAnalyzer.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { DataAnalyzerService } from './dataAnalyzer.service';
 
 @Module({
-  imports: [],
-  controllers: [],
   providers: [DataAnalyzerService],
 })
 export class DataAnalyzerModule {}
