dca1c0b8e2e984d2d5bbd271f032c9a9a6032269
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:02:54 +0200

    improve `logger`

--- shared/src/middlewares/logger.middleware.ts
+++ shared/src/middlewares/logger.middleware.ts
@@ -1,6 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function logger(req: Request, res: Response, next: NextFunction): void {
-  console.log(`Request...`);
+  const request = {
+    ip: req.ip,
+    method: req.method,
+    url: req.url.split('?')[0],
+    query: req.query,
+    params: req.params,
+  };
+
+  console.log('Request', request);
+
   next();
 }
