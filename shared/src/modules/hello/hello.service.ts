615ffa186bf7d95d74083ea1602a008cfd6f5941
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:45:28 +0200

    improve `HelloService.getHello`

--- shared/src/modules/hello/hello.service.ts
+++ shared/src/modules/hello/hello.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class HelloService {
-  private message = `Hello ${process.env.npm_package_name}!`;
+  private message = `Hello! It's ${process.env.npm_package_name}!`;
 
   public getHello(): string {
     return this.message;
