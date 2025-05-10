c05b05af5ac183001e663e725f14ef92cbe024e8
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:47:01 +0200

    add `GlobalExceptionFilter` to `s1DataAnalyzer`

--- services/s1DataAnalyzer/src/main.ts
+++ services/s1DataAnalyzer/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { GlobalExceptionFilter } from '../../../shared/src/exceptionFilters/globalException.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT);
 }
 
