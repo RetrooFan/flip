73cc2afc04b268f8bba77fd74d7f73b6d82521c7
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:39:51 +0200

    add `s0DataProvider` service code

--- services/s0DataProvider/src/main.ts
+++ services/s0DataProvider/src/main.ts
@@ -1,1 +1,10 @@
-console.log('START');
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(process.env.PORT);
+}
+
+bootstrap();
