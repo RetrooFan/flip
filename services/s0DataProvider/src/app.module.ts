58bb6ecad41f4a988ecba03c1bc9e9dceaaaebb9
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:40:37 +0200

    add `ConfigModule`

--- services/s0DataProvider/src/app.module.ts
+++ services/s0DataProvider/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DataFacilitatorModule } from './dataFacilitator/dataFacilitator.module';
 import { DataFetcherModule } from './dataFetcher/dataFetcher.module';
 import { HelloModule } from './helloModule/hello.module';
 
 @Module({
-  imports: [HelloModule, DataFetcherModule, DataFacilitatorModule]
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HelloModule, DataFetcherModule, DataFacilitatorModule],
 })
 export class AppModule {}
