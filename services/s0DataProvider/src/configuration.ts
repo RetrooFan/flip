f44d3debc4998a2bc1988f0381c2ddb769f37ffe
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:48:13 +0200

    improve `configuration.ts`

--- services/s0DataProvider/src/configuration.ts
+++ services/s0DataProvider/src/configuration.ts
@@ -3,6 +3,7 @@ import { ConfigObject } from '@nestjs/config';
 export default (): ConfigObject => {
   return {
     nodeEnv: process.env.NODE_ENV,
+    packageName: process.env.npm_package_name,
     port: parseInt(process.env.PORT),
     dataSourceApi: process.env.DATA_SOURCE_API,
     itemsNumberQueryLimit: parseInt(process.env.ITEMS_NUMBER_QUERY_LIMIT),
--- services/s1DataAnalyzer/src/configuration.ts
+++ services/s1DataAnalyzer/src/configuration.ts
@@ -3,6 +3,7 @@ import { ConfigObject } from '@nestjs/config';
 export default (): ConfigObject => {
   return {
     nodeEnv: process.env.NODE_ENV,
+    packageName: process.env.npm_package_name,
     port: parseInt(process.env.PORT),
     dataAnalyzerCronTime: process.env.DATA_ANALYZER_CRON_TIME,
     mongoDbUriFlip: process.env.MONGODB_URI_FLIP.replace('?????', process.env.NODE_ENV),
