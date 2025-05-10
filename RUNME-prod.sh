fea6f97c0de673501734541667e9ff0b01f955cd
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:06:00 +0200

    improve start scripts

--- RUNME-prod.sh
+++ RUNME-prod.sh
@@ -5,4 +5,5 @@ S0_DATA_PROVIDER_PORT_DEBUGGER=4000 \
 S2_METRICS_PROVIDER_PORT=3002 \
 S2_METRICS_PROVIDER_PORT_DEBUGGER=4002 \
 docker-compose up --renew-anon-volumes
+#docker-compose up --renew-anon-volumes --no-start
 #docker-compose down
