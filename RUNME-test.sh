53dd9d340d6a9b25d73112087fe7f74eb236d02c
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:06:16 +0200

    improve `RUNME-test.sh`

--- RUNME-test.sh
+++ RUNME-test.sh
@@ -2,4 +2,5 @@
 SERVICE=all \
 DEBUGGER_PORT=9229 \
 docker-compose --file docker-compose.test.yml up --abort-on-container-exit --renew-anon-volumes
+# docker-compose --file docker-compose.test.yml up --abort-on-container-exit --renew-anon-volumes --no-start
 # docker-compose --file docker-compose.test.yml down
