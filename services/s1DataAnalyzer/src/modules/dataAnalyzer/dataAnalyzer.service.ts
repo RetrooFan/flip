05fdb8d1de7c54b8ba0543f1e79abb3bd61e3b1f
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:49:04 +0200

    add logic to `DataAnalyzerService.dataAnalyzerCronCallback`

--- services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.service.ts
+++ services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.service.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AmountOfOrders, AmountOfOrdersDocument } from '../../../../../shared/src/entities/amountOfOrders.entity';
+import { Order } from '../../../../../shared/src/entities/order.entity';
 import { DbConnection } from '../../../../../shared/src/enums/dbConnection.enum';
 import { AxiosService } from '../../../../../shared/src/modules/axios/axios.service';
 import { CronService } from '../../../../../shared/src/modules/cron/cron.service';
@@ -36,7 +37,25 @@ export class DataAnalyzerService {
   }
 
   private async dataAnalyzerCronCallback(): Promise<void> {
-    console.log(this.message, DataAnalyzerService.name);
+    const currentAmount = await this.amountOfOrdersModel.findOne();
+    const currentValue = currentAmount ? currentAmount.value : 0;
+
+    const limit = this.configService.get<number>('itemsNumberQueryLimit');
+    const page = Math.trunc(currentValue / limit) + 1;
+
+    const baseURL = this.configService.get<string>('dataSourceApi');
+    const url = 'orders';
+    const params = { _page: page, _limit: limit };
+
+    const data = await this.axiosService.request<Order[]>({ baseURL, url, params });
+    const value = (page - 1) * limit + data.length;
+    const counter = currentValue % limit;
+
+    for (let i = counter; i < data.length; i++) {}
+
+    if (value > currentValue && data.length) {
+      new this.amountOfOrdersModel({ value }).save();
+    }
   }
 
   private async productYesterdayCounterCallback(): Promise<void> {
