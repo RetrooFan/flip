c072d922f00362ebdc4f3302b5533daf37a085b8
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:45:06 +0200

    fix expected `this` eslint error

--- shared/src/modules/hello/hello.service.ts
+++ shared/src/modules/hello/hello.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export default class HelloService {
+  private message = `Hello ${process.env.npm_package_name}!`;
+
   public getHello(): string {
-    return `Hello ${process.env.npm_package_name}!`;
+    return this.message;
   }
 }
