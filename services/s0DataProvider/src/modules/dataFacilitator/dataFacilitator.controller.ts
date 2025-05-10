9acf22db03a738f4ad40c795226eb03b8b423c40
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:43:34 +0200

    implement `DataFacilitatorModule`

--- services/s0DataProvider/src/modules/dataFacilitator/dataFacilitator.controller.ts
+++ services/s0DataProvider/src/modules/dataFacilitator/dataFacilitator.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { DataFacilitatorQueryDto } from './dtos/dataFacilitatorQuery.dto';
+import { DataFacilitatorService } from './dataFacilitator.service';
+import { Order } from '../../../../../shared/src/entities/order.entity';
 
 @Controller()
-export class DataFacilitatorController {}
+export class DataFacilitatorController {
+  constructor(private readonly dataFacilitatorService: DataFacilitatorService) {}
+
+  @Get('orders')
+  private async gerOrders(
+    @Query('_page', ParseIntPipe) _page: number,
+    @Query('_limit', ParseIntPipe) _limit: number,
+  ): Promise<Order[]> {
+    const dataFacilitatorQueryDto: DataFacilitatorQueryDto = { _page, _limit };
+
+    const orders = await this.dataFacilitatorService.getOrders(dataFacilitatorQueryDto);
+
+    return orders;
+  }
+}
--- services/s0DataProvider/src/modules/dataFacilitator/dataFacilitator.service.ts
+++ services/s0DataProvider/src/modules/dataFacilitator/dataFacilitator.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DataFacilitatorQueryDto } from './dtos/dataFacilitatorQuery.dto';
+import { Order, OrderDocument } from '../../../../../shared/src/entities/order.entity';
+import { DbConnection } from '../../../../../shared/src/enums/dbConnection.enum';
 
 @Injectable()
-export class DataFacilitatorService {}
+export class DataFacilitatorService {
+  constructor(
+    @InjectModel(Order.name, DbConnection.DataFacilitator)
+    private readonly orderModel: Model<OrderDocument>,
+  ) {}
+
+  public async getOrders(dataFacilitatorQueryDto: DataFacilitatorQueryDto): Promise<Order[]> {
+    const skipItemsNumber = dataFacilitatorQueryDto._limit * (dataFacilitatorQueryDto._page - 1);
+
+    return await this.orderModel.find().skip(skipItemsNumber).limit(dataFacilitatorQueryDto._limit);
+  }
+}
