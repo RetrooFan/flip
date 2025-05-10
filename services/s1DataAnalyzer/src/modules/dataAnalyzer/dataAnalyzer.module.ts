1311e7658d1b65e96bfdf37b0f836413f9095fa1
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:46:38 +0200

    add cron job

--- services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.module.ts
+++ services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CronModule } from '../cron/cron.module';
 import { DataAnalyzerService } from './dataAnalyzer.service';
 
 @Module({
+  imports: [CronModule],
   providers: [DataAnalyzerService],
 })
 export class DataAnalyzerModule {}
--- services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.service.ts
+++ services/s1DataAnalyzer/src/modules/dataAnalyzer/dataAnalyzer.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CronService } from '../cron/cron.service';
 
 @Injectable()
-export class DataAnalyzerService {}
+export class DataAnalyzerService {
+  constructor(cronService: CronService) {
+    cronService.addJob(DataAnalyzerService.name, process.env.DATA_ANALYZER_CRON_TIME, analyzeData);
+  }
+}
+
+async function analyzeData(): Promise<void> {
+  console.log('BLA', DataAnalyzerService.name);
+}
