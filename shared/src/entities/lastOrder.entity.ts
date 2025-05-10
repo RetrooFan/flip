e04d84d610aeb74a8c22f4e7ccc11e794f78a367
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:48:20 +0200

    improve `LastOrder`

--- shared/src/entities/lastOrder.entity.ts
+++ shared/src/entities/lastOrder.entity.ts
@@ -6,7 +6,7 @@ export type LastOrderDocument = LastOrder & Document;
 @Schema({ timestamps: true, capped: { max: 1, size: 1 } })
 export class LastOrder {
   @Prop({ required: true })
-  number: number;
+  index: number;
 }
 
 export const lastOrderSchema = SchemaFactory.createForClass(LastOrder);
