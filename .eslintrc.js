ab9b3f2fb0ac49a2ea600dd3b1dedaa9d3cc6cba
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:44:22 +0200

    remove `@typescript-eslint/interface-name-prefix` eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,20 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': ['PascalCase'],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ],
     'import/prefer-default-export': 'off',
     'max-classes-per-file': 'off',
     'class-methods-use-this': 'off',
