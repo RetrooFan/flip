5a6843a39b037d6faa532e2b2f946b86c5453b5d
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 12:05:56 +0200

    add `HelloService` tests

---
+++ shared/src/modules/hello/hello.service.spec.ts
@@ -0,0 +1,37 @@
+import { ConfigModule, ConfigObject, ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { HelloService } from './hello.service';
+
+describe('HelloService', () => {
+  let helloService: HelloService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [
+            (): ConfigObject => ({
+              packageName: 'packageName',
+            }),
+          ],
+        }),
+      ],
+      providers: [HelloService, ConfigService],
+    }).compile();
+
+    helloService = app.get<HelloService>(HelloService);
+  });
+
+  it('should be defined', () => {
+    expect(helloService).toBeDefined();
+  });
+
+  describe('getHello', () => {
+    it('should return proper message', () => {
+      console.log('HelloService.getHello test', process.env.npm_package_name);
+
+      expect(helloService.getHello()).toBe(`Hello! It's packageName!`);
+    });
+  });
+});
