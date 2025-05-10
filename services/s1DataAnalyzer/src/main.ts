d2a4af4cc2ff9736560e9b5300f9e5ab12a120df
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:48:00 +0200

    utilize `ConfigService`

--- services/s1DataAnalyzer/src/main.ts
+++ services/s1DataAnalyzer/src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FlipExceptionFilter } from '../../../shared/src/exceptionFilters/flipException.filter';
 import { AppModule } from './app.module';
@@ -5,7 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new FlipExceptionFilter(app));
-  await app.listen(process.env.PORT);
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('port');
+  await app.listen(port);
 }
 
 bootstrap();
