5e04dedd5df0d38418a38d796386f90c280338b5
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:41:05 +0200

    add `findNumberOfItems`

--- services/s0DataProvider/src/modules/dataFetcher/dataFetcher.service.ts
+++ services/s0DataProvider/src/modules/dataFetcher/dataFetcher.service.ts
@@ -5,4 +5,38 @@ export class DataFetcherService {
   public loadData(): string {
     return 'Data loading started!';
   }
+
+  private async findNumberOfItems(): Promise<number> {
+    let data: unknown[] = [{}];
+    let page = 150;
+    let previousPage = 150;
+    let delta = 10;
+    const limit = 100;
+
+    while (1) {
+      const params = { _page: page, _limit: limit };
+
+      ({ data } = await this.axiosInstance.get('orders', {
+        params,
+        baseURL: process.env.DATA_SOURCE_API,
+      }));
+
+      console.log('page', page);
+      console.log('data.length', data.length);
+
+      if (data.length === limit) {
+        previousPage = page;
+        page += delta;
+      } else if (data.length) {
+        const itemsNumber = (page - 1) * limit + data.length;
+
+        console.log(itemsNumber);
+
+        return itemsNumber;
+      } else {
+        delta = Math.trunc(delta / 2);
+        page = previousPage + delta;
+      }
+    }
+  }
 }
