0e8e71a47f27c91e729e55002a48f22f651416de
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-10 11:48:56 +0200

    add env

--- services/s1DataAnalyzer/src/configuration.ts
+++ services/s1DataAnalyzer/src/configuration.ts
@@ -6,9 +6,10 @@ export default (): ConfigObject => {
     packageName: process.env.npm_package_name,
     port: parseInt(process.env.PORT),
     dataAnalyzerCronTime: process.env.DATA_ANALYZER_CRON_TIME,
+    dataSourceApi: process.env.DATA_SOURCE_API.replace('?????', process.env.NODE_ENV),
+    itemsNumberQueryLimit: parseInt(process.env.ITEMS_NUMBER_QUERY_LIMIT),
     mongoDbUriFlip: process.env.MONGODB_URI_FLIP.replace('?????', process.env.NODE_ENV),
     productYesterdayCounterTime: process.env.PRODUCT_YESTERDAY_COUNTER_TIME,
     redisUrl: process.env.REDIS_URL.replace('?????', process.env.NODE_ENV),
-    dataSourceApi: process.env.DATA_SOURCE_API.replace('?????', process.env.NODE_ENV),
   };
 };
